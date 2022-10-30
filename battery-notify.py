c58c80cd574dacd12c6923429b3de821efc701b2
Owen Singh <owensingh72@gmail.com>
2022-10-30 15:05:57 +0530

    fixes

--- battery-notify.py
+++ battery-notify.py
@@ -7,10 +7,11 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
+from functools import lru_cache
 
-your_email = 'example@icloud.com'
-send_to = 'example@gmail.com'
-app_password = 'app-password-from-icloud-site'
+your_email = 'youremail@icloud.com'
+send_to = 'youremail@gmail.com'
+app_password = 'your-app-pass-here'
 
 msg = MIMEMultipart()
 msg['From'] = your_email
@@ -18,6 +19,10 @@ msg['To'] = send_to
 msg['Subject'] = 'Your Mac is Successfully Charged :)'
 message = 'Please Disconnect your charger, Your Mac Device is successfully charged. Disconnecting your Charger on time can help you increase the lifespan of your battery <3'
 msg.attach(MIMEText(message))
+
+state = {
+	"sent": False
+}
 
 # Tip: When you are away from your Mac, and you have your mobile near you.
 # 1. Make sure to turn on Email Sync on mobile network, So you get instant notification.
@@ -35,21 +40,24 @@ def sendemail():
 		return False
 
 # Toast Notifications
-title = "Battery Successfully Charged :)"
-message = "Please Disconnect your Charger, This will help you to improve your battery life!"
-send = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
-
+def notisend():
+	title = "Battery Successfully Charged :)"
+	message = "Please Disconnect your Charger, This will help you to improve your battery life!"
+	send = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
+	os.system(send)
+	
 def charger():
 	threading.Timer(60, charger).start()
 	battery = psutil.sensors_battery()
 	if battery.power_plugged == False:
 		print("Not connected")
 	else:
-		batterycheck(battery)
+		not state["sent"] and batterycheck(battery)
 
 def batterycheck(battery):
 	if battery.percent == 100:
-		os.system(send)
+		state["sent"] = True
+		notisend()
 		sendemail() and print("Successfully Charged")
 	else:
 		charger()
