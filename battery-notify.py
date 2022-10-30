7ecd11373d44685c5ad49249295256c8b64dfbef
Owen Singh <owensingh72@gmail.com>
2022-10-30 19:12:55 +0530

    bug fixes, recursion fix, stack fix

--- battery-notify.py
+++ battery-notify.py
@@ -10,7 +10,7 @@ import os
 
 your_email = 'youremail@icloud.com'
 send_to = 'youremail@gmail.com'
-app_password = 'your-app-pass-here'
+app_password = 'your-apple-app-pass'
 
 msg = MIMEMultipart()
 msg['From'] = your_email
@@ -20,45 +20,55 @@ message = 'Please Disconnect your charger, Your Mac Device is successfully charg
 msg.attach(MIMEText(message))
 
 state = {
-	"sent": False
+    "sent": False
 }
 
 # Tip: When you are away from your Mac, and you have your mobile near you.
 # 1. Make sure to turn on Email Sync on mobile network, So you get instant notification.
+
+
 def sendemail():
-	try:
-		mailserver = smtplib.SMTP('smtp.mail.me.com', 587)
-		mailserver.starttls()
-		mailserver.ehlo()
-		mailserver.login(your_email, app_password)
-		mailserver.sendmail(your_email, send_to, msg.as_string())
-		mailserver.quit()
-		return True
-	except:
-		print("Something went wrong with the email service, Make sure your password and email are correct.")
-		return False
+    try:
+        mailserver = smtplib.SMTP('smtp.mail.me.com', 587)
+        mailserver.starttls()
+        mailserver.ehlo()
+        mailserver.login(your_email, app_password)
+        mailserver.sendmail(your_email, send_to, msg.as_string())
+        mailserver.quit()
+        return True
+    except:
+        print("Something went wrong with the email service, Make sure your password and email are correct.")
+        return False
 
 # Toast Notifications
+
+
 def notisend():
-	title = "Battery Successfully Charged :)"
-	message = "Please Disconnect your Charger, This will help you to improve your battery life!"
-	send = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
-	os.system(send)
-	
+    title = "Battery Successfully Charged :)"
+    message = "Please Disconnect your Charger, This will help you to improve your battery life!"
+    send = f''' osascript -e 'display notification "{message}" with title "{title}"' '''
+    os.system(send)
+
+
 def charger():
-	threading.Timer(60, charger).start()
-	battery = psutil.sensors_battery()
-	if battery.power_plugged == False:
-		print("Not connected")
-	else:
-		not state["sent"] and batterycheck(battery)
+    while True:
+        time.sleep(60)
+        battery = psutil.sensors_battery()
+        if battery.power_plugged == False:
+            print("Not connected")
+        else:
+            not state["sent"] and batterycheck(battery)
+
 
 def batterycheck(battery):
-	if battery.percent == 100:
-		state["sent"] = True
-		notisend()
-		sendemail() and print("Successfully Charged")
-	else:
-		charger()
+    if battery.percent == 100:
+        state["sent"] = True
+        notisend()
+        sendemail() and print("Successfully Charged")
+    else:
+        charger()
 
-charger()
+
+c = threading.Thread(target=charger)
+c.start()
+c.join()
---
+++ personal.py
@@ -0,0 +1,3 @@
+your_email = 'owensingh72@icloud.com'
+send_to = 'owensingh72@gmail.com'
+app_password = 'qlzw-gqey-jspa-nxbl'
